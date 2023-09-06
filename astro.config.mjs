89440b5b17257c423881be2fecb9cc41c0b6c9cb
Rob <rob.malyon@gmail.com>
2023-09-06 22:38:35 +0100

    First react component

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config';
-
 import netlify from "@astrojs/netlify/functions";
+import tailwind from "@astrojs/tailwind";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: netlify()
+  adapter: netlify(),
+  integrations: [tailwind(), react()]
 });
